f2529388233b00ad5a68c87687c5c5c039b52a99
Alexander <rogachevaimsc@gmail.com>
2020-06-02 14:15:00 +0300

    plots added

--- Practice/homework_6/methods.py
+++ Practice/homework_6/methods.py
@@ -74,37 +74,6 @@ def get_line_search_tool(line_search_options=None):
         return LineSearchTool()
 
 
-# class History(object):
-#     def __init__(self, trace):
-#         self.trace = trace
-#         self.t = time.time()
-#         if not trace:
-#             self.history = None
-#             return
-#         self.history = defaultdict(list)
-#
-#     def add_record_to_history(self, func_val, grad_norm, x):
-#         if not self.trace:
-#             return
-#         self.history['time'].append(time.time() - self.t)
-#         self.history['func'].append(func_val)
-#         self.history['grad_norm'].append(grad_norm)
-#         if x.size <= 2:
-#             self.history['x'].append(x)
-#         self.history['x_star'] = x
-
-
-# def add_record_to_history(self):
-#     now = datetime.now()
-#     self.time += (now - self._absolute_time).total_seconds()
-#     self._absolute_time = now
-#     self.hist['func'].append(self.oracle.func(self.x_k))
-#     self.hist['time'].append(self.time)
-#     if not hasattr(self, 'grad_k'):
-#         self.grad_k = self.oracle.grad(self.x_k)
-#     self.hist['grad_norm'].append(npla.norm(self.grad_k))
-
-
 class Newton(object):
     def __init__(self, oracle, x_0, tolerance=1e-4, line_search_options=None):
         self.oracle = oracle
@@ -112,11 +81,6 @@ class Newton(object):
         self.line_search_tool = get_line_search_tool(line_search_options)
         self.hist = defaultdict(list)
         self.x_0 = x_0
-        if self.line_search_tool._method == 'Constant':
-            self.alpha_0 = self.line_search_tool.c
-        else:
-            self.alpha_0 = 1.
-
 
     def add_record_to_history_(self):
         now = datetime.now()
@@ -149,10 +113,9 @@ class Newton(object):
                 message = "success"
                 break
             d = get_d(self.x_k, self.grad_k, self.oracle)
-            alpha = self.line_search_tool.line_search(self.oracle, self.x_k, d,self.alpha_0)
+            alpha = self.line_search_tool.line_search(self.oracle, self.x_k, d)
             self.x_k = self.x_k + d * alpha
         self.hist['x_star'] = self.x_k.copy()
-
         return self.x_k, message, self.hist
 
 
@@ -184,14 +147,14 @@ class BFGS(object):
         if self.x_k.size <= 2:
             self.hist['x'].append(self.x_k)
 
-    def update_H(self):
+    def calc_H(self):
         self.s_k = (self.x_k - self.x_k_).reshape((self.x_k.shape[0], 1))
         self.y_k = (self.grad_k - self.grad_k_).reshape((self.x_k.shape[0], 1))
-        ml = self.s_k.flatten().dot(self.y_k.flatten())
-        c = 1e-8
-        self.H_k -= self.H_k.dot(self.y_k).dot(self.s_k.T) / (ml + c)
-        self.H_k -= (self.H_k.dot(self.y_k).dot(self.s_k.T) / (ml + c)).T
-        self.H_k += self.s_k.dot(self.s_k.T) / (ml + c)
+        ro = self.s_k.flatten().dot(self.y_k.flatten())
+        c = 1e-4
+        self.H_k -= self.H_k.dot(self.y_k).dot(self.s_k.T) / (ro + c)
+        self.H_k -= (self.H_k.dot(self.y_k).dot(self.s_k.T) / (ro + c)).T
+        self.H_k += self.s_k.dot(self.s_k.T) / (ro + c)
 
     def run(self, max_iter=100):
         message = 'ok'
@@ -211,7 +174,7 @@ class BFGS(object):
             alpha_k = self.line_search_tool.line_search(self.oracle, self.x_k, d_k, self.alpha_0)
             self.x_k = self.x_k + alpha_k * d_k
             self.grad_k = self.oracle.grad(self.x_k)
-            self.update_H()
+            self.calc_H()
         self.hist['x_star'] = self.x_k.copy()
         return self.x_k, message, self.hist
 
@@ -250,8 +213,8 @@ class LBFGS(object):
         if len(memory) == 0:
             return gamma * v
         s, y = memory[-1]
-        v1 = v - s.dot(v) / y.dot(s) * y
-        z = self.lbfgs_mul(v1, memory[:-1], gamma)
+        v_next = v - s.dot(v) / y.dot(s) * y
+        z = self.lbfgs_mul(v_next, memory[:-1], gamma)
         return z + (s.dot(v) - y.dot(z)) / y.dot(s) * s
 
     def run(self, max_iter=100):
@@ -282,21 +245,3 @@ class LBFGS(object):
             self.lbfgs_queue.append((self.x_k - self.x_k_, self.oracle.grad(self.x_k) - self.grad_k_))
         self.hist['x_star'] = self.x_k.copy()
         return self.x_k, message, self.hist
-
-#
-# def lbfgs_direction(H, grad):
-#     s, y = H[0]
-#     gamma = y.dot(s) / y.dot(y)
-#     return bfgs_multiply(-grad, H, 0, gamma)
-#
-#
-# def bfgs_multiply(v, H, index, gamma):
-#     if index == len(H):
-#         return gamma * v
-#     s, y = H[index]
-#     s_dot_v = s.dot(v)
-#     y_dot_s = y.dot(s)
-#     v_next = v - s_dot_v / y_dot_s * y
-#     z = bfgs_multiply(v_next, H, index + 1, gamma)
-#     return z + (s_dot_v - y.dot(z)) / y_dot_s * s
-# your code here
